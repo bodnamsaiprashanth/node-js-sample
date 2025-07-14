e44e6825c17d497c79f06d6aec3fc05884c1d2be
root <root@ip-172-31-83-178.ec2.internal>
2025-07-14 04:11:24 +0000

    🛠️ Test CI/CD webhook auto-deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello from Sai’s automated CI/CD pipeline! 🚀')
 })
 
 app.listen(app.get('port'), function() {
